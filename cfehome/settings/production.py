54662d406a77b32a4b196401f7312df5ce9bf021
Alexander Kochergin <kocherginalexander@gmail.com>
2017-09-13 20:32:33 -0700

    Initial Heroku commit

--- cfehome/settings/production.py
+++ cfehome/settings/production.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'rbo#8bf6p*_8-n&$xuf!xp#%-1+u)2tj%@-$b!h^e17usr0_3b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kochblog.herokuapp.com', '.kochblog.com']
 
 
 # Application definition
@@ -130,3 +130,11 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
+
